214a8adc457a872f611a33be14085cce04c01fde
Calvin Park <calvinspark@gmail.com>
2024-12-25 16:34:55 -0800

    Keyboard entry v0.1.0

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/bash
 
-exec ./launch_chffrplus.sh
+# Next reboot should trigger an install without a reset
+sudo rm /data/continue.sh
+
+# Get Python working
+ln -sfn $(pwd) /data/pythonpath
+export PYTHONPATH="$PWD"
+
+# Make it possible to write to /persist/tsk
+trap "sudo mount -o remount,ro /persist" EXIT
+sudo mount -o remount,rw /persist
+sudo mkdir -p /persist/tsk || true
+sudo chown comma /persist/tsk
+
+# Run TSK Manager
+python3 tsk/kbd.py
+#bash # Debug
+
+# Clean up
+sudo rm -rf /data/openpilot
+# And done
+sudo reboot
+
+#exec ./launch_chffrplus.sh
